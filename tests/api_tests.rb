d8622c907dc5c6accb7f5995a9696c190694b501
zombyjab <jab.leo@gmail.com>
2018-03-09 11:06:15 -0500

    error api test

--- tests/api_tests.rb
+++ tests/api_tests.rb
@@ -3,6 +3,6 @@ require 'airborne'
 describe 'reloj api' do
   it 'retorna si debe sonar o no la alarma' do
     get 'https://relojdespertador.herokuapp.com/api/alarma/sonar?hora-alarma=07:20&hora-actual=07:19'
-    expect_json(sonar: false)
+    expect_json(sonar: true)
   end
 end
